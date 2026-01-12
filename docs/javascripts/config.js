5b2a85b8dc58fc482dfc4eec5a71cf066351be64
Damian <damian.ch.82@gmail.com>
2026-01-12 20:52:31 +0100

    Refactor MathJax configuration to simplify inline math settings

--- docs/javascripts/config.js
+++ docs/javascripts/config.js
@@ -1,12 +1,5 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\\\[", "\\\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
   }
 };
